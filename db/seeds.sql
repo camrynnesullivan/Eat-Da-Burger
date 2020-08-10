46b2da3ad8f910003c5170780f0323f4f4168f76
Camrynne Sullivan <camrynne.sullivan@gmail.com>
2020-08-09 20:03:05 -0400

    Eat-Da-Burger App

---
+++ db/seeds.sql
@@ -0,0 +1,29 @@
+INSERT INTO
+    burgers (name)
+VALUES
+    ('Vegan Burger');
+
+INSERT INTO
+    burgers (name)
+VALUES
+    ('Cheese Burger');
+
+INSERT INTO
+    burgers (name)
+VALUES
+    ('Bacon Cheese Burger');
+
+INSERT INTO
+    burgers (name)
+VALUES
+    ('Sullivan Burger');
+
+INSERT INTO
+    burgers (name)
+VALUES
+    ('Doyle Burger');
+
+INSERT INTO
+    burgers (name)
+VALUES
+    ('Grilled Cheese');
