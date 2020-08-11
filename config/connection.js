2c47203cb2e7356d80d8987bb5180f1e079ed51b
Camrynne Sullivan <camrynne.sullivan@gmail.com>
2020-08-10 20:30:39 -0400

    Added dotenv

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,20 @@
 // Set up MySQL connection.
+require("dotenv").config();
 var mysql = require("mysql");
+var connection;
+if (process.env.JAWSDB_URL) {
+  connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  connection = mysql.createConnection(process.env.DB_URL);
+}
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "abundance26$",
-  database: "burgers_db",
-});
+// var connection = mysql.createConnection({
+//   host: "localhost",
+//   port: 3306,
+//   user: "root",
+//   password: "",
+//   database: "burgers_db",
+// });
 
 // Make connection.
 connection.connect(function (err) {
--- db/schema.sql
+++ db/schema.sql
@@ -1,10 +1,37 @@
-CREATE DATABASE burgers_db;
-USE burgers_db;
+USE scmchcyqpry0cycb;
 
-CREATE TABLE burgers
-(
+CREATE TABLE burgers (
 	id int NOT NULL AUTO_INCREMENT,
 	name varchar(255) NOT NULL,
 	devoured BOOLEAN DEFAULT false,
 	PRIMARY KEY (id)
 );
+INSERT INTO
+    burgers (name)
+VALUES
+    ('Vegan Burger');
+
+INSERT INTO
+    burgers (name)
+VALUES
+    ('Cheese Burger');
+
+INSERT INTO
+    burgers (name)
+VALUES
+    ('Bacon Cheese Burger');
+
+INSERT INTO
+    burgers (name)
+VALUES
+    ('Sullivan Burger');
+
+INSERT INTO
+    burgers (name)
+VALUES
+    ('Doyle Burger');
+
+INSERT INTO
+    burgers (name)
+VALUES
+    ('Grilled Cheese');
